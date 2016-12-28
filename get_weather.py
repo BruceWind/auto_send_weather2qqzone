d8a81548097abde129cbf1e99b72a6361455f35d
weizongwei5 <weizongwei5@gmail.com>
2016-12-28 20:34:57 +0800

    test

--- get_weather.py
+++ get_weather.py
@@ -1,17 +1,23 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-
 import requests
 import json
+url = r'http://wthrcdn.etouch.cn/weather_mini?citykey=101021300'
+jsonStr = requests.get(url).text
+data = eval(json.dumps(jsonStr,ensure_ascii=False))
+exec("Data="+data)
+info = Data["data"]
+weather = info["forecast"]
+print(info['city'].decode("utf-8"))
+forecast = info['forecast']
 
-url="http://wthrcdn.etouch.cn/weather_mini?citykey=101021300"
-
-def getHtml():
-	return requests.get(url).text
-
-data = json.loads(getHtml())
-weather = data["data"]
-
-print ("city:",weather["city"])
-print ("prompt",weather["ganmao"])
+today = forecast[0]
+print (today["date"].decode("utf-8"))
+print (today["high"].decode("utf-8"))
+print (today["low"].decode("utf-8"))
+print (today["type"].decode("utf-8"))
+print (today["fengxiang"].decode("utf-8"))
+today = forecast[1]
+print (today["date"].decode("utf-8"))
+print (today["high"].decode("utf-8"))
+print (today["low"].decode("utf-8"))
+print (today["type"].decode("utf-8"))
+print (today["fengxiang"].decode("utf-8"))
