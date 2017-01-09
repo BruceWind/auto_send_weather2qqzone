ba3a711ff308fc8cfbd186f845e806c77d7386e9
weizongwei5 <weizongwei5@gmail.com>
2017-01-09 14:33:52 +0800

    weather and  aqi

---
+++ get_aqi_for_city.py
@@ -0,0 +1,29 @@
+# encoding: utf-8
+#!/usr/bin/env python
+
+import requests
+import json
+class AQIGetter(object):
+
+
+# 返回未转码的 字符串
+    def getAQIStr(self,city):
+        url = r'http://www.pm25.in/api/querys/pm2_5.json?city='+unicode(city, "utf-8")+'&token=5j1znBVAsnSf5xQyNQyq&avg'
+        # url = r'http://www.pm25.in/api/querys/pm2_5.json?city=%E4%BA%B3%E5%B7%9E&token=5j1znBVAsnSf5xQyNQyq&avg'
+        jsonStr = requests.get(url).text
+
+    # print jsonStr
+
+        apiarr = json.loads(jsonStr)
+        if(len(apiarr) > 0):
+            lastobj=apiarr[len(apiarr)-1]
+            area =lastobj['area']
+            aqi = str(lastobj['aqi']).encode("utf-8")
+            pm2_5_24h=str(lastobj['pm2_5_24h'])
+            quality = lastobj['quality']
+
+        # str = area+"\tAQI:"+aqi+" \t 各监测点平均PM2.5:"+pm2_5_24h +"\t "+quality
+            return area+'\t\tAQI:'+aqi +u'\t\tPM2.5平均值：'.encode('utf-8').decode('utf-8')+pm2_5_24h+'\t\t'+quality
+
+
+print  AQIGetter().getAQIStr('shanghai')
