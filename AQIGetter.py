014ce5c8d70d4df35b9bb528080b8d9610028930
weizongwei5 <weizongwei5@gmail.com>
2017-01-13 19:22:11 +0800

    解决 crontab下 AQI无法使用

--- AQIGetter.py
+++ AQIGetter.py
@@ -8,22 +8,21 @@ class AQIGetter(object):
 
 # 返回未转码的 字符串
     def getAQIStr(self,city):
-        url = r'http://www.pm25.in/api/querys/pm2_5.json?city='+unicode(city, "utf-8")+'&token=fVtxPUxgpzQTnVNta5JL'
+        url = r'http://www.pm25.in/api/querys/pm2_5.json?city='+unicode(city, "utf-8")+'&token=fVtxPUxgpzQTnVNta5JL&stations=no'
         # url = r'http://www.pm25.in/api/querys/pm2_5.json?city=%E4%BA%B3%E5%B7%9E&token=5j1znBVAsnSf5xQyNQyq&avg'
-        jsonStr = requests.get(url).text
+        temp = requests.get(url).text
+        l=len(temp)-1
+        jsonStr=temp[1:l]
+        data = eval(json.dumps(jsonStr,ensure_ascii=False))
+        exec("Data="+data)
+        area =Data['area']
+        aqi = str(Data['aqi']).encode('utf-8')
+        pm2_5_24h=str(Data['pm2_5_24h']).encode('utf-8')
+        quality = Data['quality']
 
-    # print jsonStr
+    # str = area+"\tAQI:"+aqi+" \t 各监测点平均PM2.5:"+pm2_5_24h +"\t "+quality
+        return area+"\tAQI:"+aqi+" \t 各监测点平均PM2.5:"+pm2_5_24h +"\t "+quality
 
-        apiarr = json.loads(jsonStr)
-        if(len(apiarr) > 0):
-            lastobj=apiarr[0]
-            area =lastobj['area']
-            aqi = str(lastobj['aqi']).encode("utf-8")
-            pm2_5_24h=str(lastobj['pm2_5_24h'])
-            quality = lastobj['quality']
-
-        # str = area+"\tAQI:"+aqi+" \t 各监测点平均PM2.5:"+pm2_5_24h +"\t "+quality
-            return area+'\t\tAQI:'+aqi +u'\t\tPM2.5平均值：'.encode('utf-8').decode('utf-8')+pm2_5_24h+'\t\t'+quality
 
 
 print  AQIGetter().getAQIStr('shanghai')
